45e3cf9c1267c19263781024433d05a95835edb4
Chintan Udani <chintanudani2000@gmail.com>
2023-03-25 10:46:56 +0530

    updated contact

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -1,74 +1,9 @@
 import React from 'react'
 
-
 const Contact = () => {
-    const [formData, setFormData] = useState({
-      name: '',
-      email: '',
-      message: ''
-    });
-  
-    const handleChange = (event) => {
-      setFormData({
-        ...formData,
-        [event.target.name]: event.target.value
-      });
-    };
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-  
-      fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: new URLSearchParams(formData).toString()
-      })
-        .then(() => {
-          alert('Thank you for your message!');
-          setFormData({
-            name: '',
-            email: '',
-            message: ''
-          });
-        })
-        .catch((error) => alert(error));
-    };
-
-
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={formData.name}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <label>
-        Email:
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <label>
-        Message:
-        <textarea
-          name="message"
-          value={formData.message}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <button type="submit">Send</button>
-    </form>
-  );
-};
+    <div>index</div>
+  )
+}
 
 export default Contact;
