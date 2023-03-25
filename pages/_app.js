7aca7d4730586c200f2dcce14c334adc0778cb04
Chintan Udani <chintanudani2000@gmail.com>
2023-03-25 20:17:44 +0530

    fixed bug #2

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import "../styles/globals.css";
 import '../components/WorkCard/style.css';
 import '../components/Skills/styles.css';
@@ -6,9 +7,16 @@ import { ThemeProvider } from "next-themes";
 
 const App = ({ Component, pageProps }) => {
   return (
+    <>
+    <Head>
+      <link rel="stylesheet" href="../components/WorkCard/style.css" />
+      <link rel="stylesheet" href="../components/Skills/styles.css" />
+      <link rel="stylesheet" href="../components/Contact/style.css" />
+    </Head>
     <ThemeProvider>
       <Component {...pageProps} />
     </ThemeProvider>
+  </>
   );
 };
 
