02d57c756eb9406f7d48d846579f48088c8b56ac
Chintan Udani <chintanudani2000@gmail.com>
2023-03-25 10:41:52 +0530

    form updated

--- components/Contact/index.js
+++ components/Contact/index.js
@@ -1,22 +1,74 @@
 import React from 'react'
 
-const Contact = () => {
-  return (
-    <>
-    <form name="contact" method="POST" netlify>
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
 
-    </>
+const Contact = () => {
+    const [formData, setFormData] = useState({
+      name: '',
+      email: '',
+      message: ''
+    });
+  
+    const handleChange = (event) => {
+      setFormData({
+        ...formData,
+        [event.target.name]: event.target.value
+      });
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+  
+      fetch('/', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: new URLSearchParams(formData).toString()
+      })
+        .then(() => {
+          alert('Thank you for your message!');
+          setFormData({
+            name: '',
+            email: '',
+            message: ''
+          });
+        })
+        .catch((error) => alert(error));
+    };
+
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Message:
+        <textarea
+          name="message"
+          value={formData.message}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <button type="submit">Send</button>
+    </form>
   );
 };
 
-export default Contact
+export default Contact;
